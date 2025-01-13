f7867258b1503c1e5f81616cedfd278fefef1297
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-01-13 10:32:10 +0700

    Refactored eslint.config.js to use the flat configuration system

--- index.js
+++ index.js
@@ -1,128 +1,124 @@
-import cypress from "./rules/cypress";
-import vue from "./rules/vue";
+import cypressRules from "./rules/cypress";
+import vueRules from "./rules/vue";
 import importRules from "./rules/import";
 
-export default {
-  extends: [
-    "@vue/typescript/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:css/recommended",
-    "plugin:prettier/recommended",
-    "plugin:promise/recommended",
-    "plugin:vue/vue3-essential",
-    "plugin:vue/vue3-recommended",
-    "plugin:vue/vue3-strongly-recommended",
-  ],
-  globals: {
-    defineEmits: "readonly",
-    defineExpose: "readonly",
-    defineProps: "readonly",
-    withDefaults: "readonly",
+export default [
+  {
+    ignores: ["dist", "node_modules"],
   },
-  ignorePatterns: ["dist", "node_modules"],
-  overrides: [
-    {
-      extends: ["plugin:cypress/recommended"],
-      files: ["**/*.cy.spec.ts"],
-      rules: cypress,
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.vue"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      parser: "vue-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
     },
-    {
-      files: [
-        "**/main.ts",
-        "**/components/{v1,v2}/index.ts",
-        "**/registerComponent.util.ts",
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      "vue": require("eslint-plugin-vue"),
+      "css": require("eslint-plugin-css"),
+      "prettier": require("eslint-plugin-prettier"),
+      "promise": require("eslint-plugin-promise"),
+    },
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "error",
+      "@typescript-eslint/explicit-module-boundary-types": "error",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          format: ["PascalCase"],
+          selector: "interface",
+        },
+        {
+          format: ["PascalCase"],
+          selector: "typeAlias",
+        },
+        {
+          format: ["camelCase", "UPPER_CASE", "PascalCase"],
+          selector: "variable",
+        },
+        {
+          format: ["PascalCase"],
+          selector: "typeLike",
+        },
+        {
+          format: ["camelCase"],
+          selector: "function",
+        },
       ],
-      rules: {
-        "vue/multi-word-component-names": "off",
-        "vue/no-reserved-component-names": "off",
-      },
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-non-null-assertion": "error",
+      "@typescript-eslint/no-shadow": "error",
+      "camelcase": "error",
+      "capitalized-comments": "error",
+      "complexity": ["error", 50],
+      "default-param-last": "error",
+      "func-style": ["error", "expression"],
+      "multiline-comment-style": "error",
+      "no-console": [
+        "error",
+        {
+          allow: ["error"],
+        },
+      ],
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+      "no-duplicate-imports": "error",
+      "no-else-return": "error",
+      "no-empty-function": "error",
+      "no-nested-ternary": ["error", { max: 2 }],
+      "no-shadow": "error",
+      "no-unneeded-ternary": "error",
+      "no-useless-return": "error",
+      "no-var": "error",
+      "operator-assignment": "error",
+      "prefer-const": "error",
+      "prefer-destructuring": [
+        "error",
+        { array: false, object: true },
+        { enforceForRenamedProperties: false },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "auto",
+          quoteProps: "consistent",
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          useTabs: false,
+        },
+      ],
+      "promise/prefer-await-to-callbacks": "error",
+      "promise/prefer-await-to-then": "error",
+      "quotes": ["error", "single", { avoidEscape: true }],
+      ...vueRules,
+      ...importRules,
     },
-    {
-      files: ["vue.config.js"],
-      rules: {
-        "@typescript-eslint/no-var-requires": "off",
-      },
+  },
+  {
+    files: ["**/*.cy.spec.ts"],
+    plugins: {
+      cypress: require("eslint-plugin-cypress"),
     },
-  ],
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
+    rules: cypressRules,
   },
-  plugins: ["@typescript-eslint", "vue"],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "error",
-    "@typescript-eslint/explicit-module-boundary-types": "error",
-    "@typescript-eslint/naming-convention": [
-      "error",
-      {
-        format: ["PascalCase"],
-        selector: "interface",
-      },
-      {
-        format: ["PascalCase"],
-        selector: "typeAlias",
-      },
-      {
-        format: ["camelCase", "UPPER_CASE", "PascalCase"],
-        selector: "variable",
-      },
-      {
-        format: ["PascalCase"],
-        selector: "typeLike",
-      },
-      {
-        format: ["camelCase"],
-        selector: "function",
-      },
+  {
+    files: [
+      "**/main.ts",
+      "**/components/{v1,v2}/index.ts",
+      "**/registerComponent.util.ts",
     ],
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/no-non-null-assertion": "error",
-    "@typescript-eslint/no-shadow": "error",
-    "camelcase": "error",
-    "capitalized-comments": "error",
-    "complexity": ["error", 50],
-    "default-param-last": "error",
-    "func-style": ["error", "expression"],
-    "multiline-comment-style": "error",
-    "no-console": [
-      "error",
-      {
-        allow: ["error"],
-      },
-    ],
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
-    "no-duplicate-imports": "error",
-    "no-else-return": "error",
-    "no-empty-function": "error",
-    "no-nested-ternary": ["error", { max: 2 }],
-    "no-shadow": "error",
-    "no-unneeded-ternary": "error",
-    "no-useless-return": "error",
-    "no-var": "error",
-    "operator-assignment": "error",
-    "prefer-const": "error",
-    "prefer-destructuring": [
-      "error",
-      { array: false, object: true },
-      { enforceForRenamedProperties: false },
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-        quoteProps: "consistent",
-        semi: true,
-        singleQuote: true,
-        tabWidth: 2,
-        useTabs: false,
-      },
-    ],
-    "promise/prefer-await-to-callbacks": "error",
-    "promise/prefer-await-to-then": "error",
-    "quotes": ["error", "single", { avoidEscape: true }],
-    ...vue,
-    ...importRules,
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "vue/no-reserved-component-names": "off",
+    },
   },
-};
+  {
+    files: ["vue.config.js"],
+    rules: {
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
+];
