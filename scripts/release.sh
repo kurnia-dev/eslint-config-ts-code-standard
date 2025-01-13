aa65135564ffb9e0f7ee10278b861649edbbceb4
Zain Kurnia <programmer-fe3@qwertysystem.net>
2025-01-13 09:03:06 +0700

    refactor: publish from dist folder

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,4 +6,6 @@ npm version patch
 # Remove the scripts, devDependencies, and dependencies sections from package.json
 node ./scripts/pre-publish.cjs
 
+cd dist
+
 npm link && npm publish --access public
